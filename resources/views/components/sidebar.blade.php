c2fb87067f08077bb5897542e0949ec1ae99bac1
Lindsay <lindsay98@live.nl>
2025-05-06 14:05:01 +0200

    removed tailwind configurations

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,1 +1,21 @@
 
+    <aside>
+        <form action="">
+                <fieldset>
+                    <label for="products">Choose a product</label>
+                    <select name="products" id="products">
+
+                    </select>
+                    <label for="units">Choose your unit</label>
+                    <select name="units" id="units">
+
+                    </select>
+                    <label for="amount">Choose your amount</label>
+                    <input type="text" name="amount" id="amount">
+                </fieldset>
+        </form>
+    </aside>
+
+
+
+
